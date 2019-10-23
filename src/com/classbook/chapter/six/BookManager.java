d97830d92da8b0db400bb3f9fd8355746e3aced7
Cheng <ccelephant2012@gmail.com>
2019-10-23 14:16:17 +0800

    create class program

---
+++ src/com/classbook/chapter/six/BookManager.java
@@ -0,0 +1,13 @@
+package com.classbook.chapter.six;
+
+/**
+ * @program: classprogram
+ * @Description:
+ * @author: Mr.Cheng
+ * @date: 2019/10/23 2:14 下午
+ */
+public class BookManager {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/com/classbook/chapter/six/Dictionary.java
@@ -0,0 +1,28 @@
+package com.classbook.chapter.six;
+
+/**
+ * @program: classprogram
+ * @Description: 继承父类Book
+ * @author: Mr.Cheng
+ * @date: 2019/10/23 2:02 下午
+ */
+public class Dictionary extends Book {
+    private int definitions;
+
+    public Dictionary(int definitions) {
+        this.definitions = definitions;
+    }
+
+    public Dictionary(String bookId, String bookName, int pages, int definitions) {
+        super(bookId, bookName, pages);
+        this.definitions = definitions;
+    }
+
+    public int getDefinitions() {
+        return definitions;
+    }
+
+    public void setDefinitions(int definitions) {
+        this.definitions = definitions;
+    }
+}
